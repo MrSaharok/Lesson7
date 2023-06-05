4fc15bc2080e3604c1f2995384eb0f7f1116388a
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-06-05 23:09:03 +0700

    Update cargo_wagon.rb

--- cargo_wagon.rb
+++ cargo_wagon.rb
@@ -1,27 +1,12 @@
 require_relative 'wagon'
 
 class CargoWagon < Wagon
-  attr_accessor :volume, :take_volume, :free_volume
 
-  def initialize(volume)
-    super
-    @volume = volume
-    @type = :cargo
-    @free_volume = volume
-    @take_volume = 0
+  def initialize(total_place)
+    super(:cargo, total_place)
   end
 
-  def take_volumes(vol)
-    if (@take_volume + vol) <= @volume
-      @take_volume += vol
-    @free_volume = volume - vol
-    end
-  end
-  def free
-    puts "Free volume: #{free_volume}/#{volume}"
-  end
-
-  def taken
-    puts "Taken volume: #{take_volume}/#{volume}"
+  def take_place(volume)
+    @used_place += volume if (@used_place + volume) <= @total_place
   end
 end
